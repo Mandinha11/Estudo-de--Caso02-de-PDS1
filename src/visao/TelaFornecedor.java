717be78544de87e51deded7be932ddea2871f41b
lucashsantosb <lucas.hsb@aluno.ifsc.edu.br>
2023-04-13 09:06:45 -0300

    Table

--- src/visao/TelaFornecedor.java
+++ src/visao/TelaFornecedor.java
@@ -5,6 +5,8 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
 import java.awt.Color;
 import javax.swing.JLabel;
 import java.awt.Font;
@@ -51,73 +53,84 @@ public class TelaFornecedor extends JFrame {
 		setBounds(100, 100, 1049, 715);
 		contentPane = new JPanel();
 		contentPane.setForeground(new Color(255, 255, 255));
-		contentPane.setBackground(new Color(203, 125, 255));
+		contentPane.setBackground(new Color(159, 0, 0));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		contentPane.setLayout(new MigLayout("", "[84.00px,grow][148px][250px][83px][250px][150px,grow][11.00][148.00px]", "[][28px][31px][][23px][25px][23px][23px][23px][25px][][334px,grow]"));
-		
+		contentPane
+				.setLayout(new MigLayout("", "[84.00px,grow][148px][250px][83px][250px][150px,grow][11.00][148.00px]",
+						"[][28px][31px][][23px][25px][23px][23px][23px][25px][][334px,grow]"));
+
 		JLabel lblNomeEmpresa = new JLabel("Nome da empresa:");
 		lblNomeEmpresa.setForeground(new Color(255, 255, 255));
 		lblNomeEmpresa.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		contentPane.add(lblNomeEmpresa, "cell 1 1,alignx right,growy");
-		
+
 		JLabel lblCpf = new JLabel("CPF:");
 		lblCpf.setForeground(new Color(255, 255, 255));
 		lblCpf.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		contentPane.add(lblCpf, "cell 3 1,alignx right,growy");
-		
+
 		txtNomeEmpresa = new JTextField();
 		contentPane.add(txtNomeEmpresa, "cell 2 1,growx,aligny center");
 		txtNomeEmpresa.setColumns(10);
-		
+
 		txtCPF = new JTextField();
 		txtCPF.setColumns(10);
 		contentPane.add(txtCPF, "cell 4 1,growx,aligny center");
-		
+
 		JLabel lblCnpj = new JLabel("CNPJ:");
 		lblCnpj.setForeground(new Color(255, 255, 255));
 		lblCnpj.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		contentPane.add(lblCnpj, "cell 1 2,alignx right,growy");
-		
+
 		txtCNPJ = new JTextField();
 		txtCNPJ.setColumns(10);
 		contentPane.add(txtCNPJ, "cell 2 2,growx,aligny center");
-		
+
 		JLabel lblTelefone = new JLabel("Telefone:");
 		lblTelefone.setForeground(new Color(255, 255, 255));
 		lblTelefone.setFont(new Font("Tahoma", Font.PLAIN, 17));
 		contentPane.add(lblTelefone, "cell 3 2,alignx right,growy");
-		
+
 		txtTelefone = new JTextField();
 		txtTelefone.setColumns(10);
 		contentPane.add(txtTelefone, "cell 4 2,growx,aligny center");
-		
+
 		JButton btnCadastrar = new JButton("Cadastrar");
+		btnCadastrar.setBackground(new Color(230, 224, 0));
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		contentPane.add(btnCadastrar, "cell 7 4,growx,aligny top");
-		
+
 		JButton btnAtualizar = new JButton("Atualizar");
+		btnAtualizar.setBackground(new Color(230, 224, 0));
 		btnAtualizar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
 		contentPane.add(btnAtualizar, "cell 7 6,growx,aligny top");
-		
+
 		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setBackground(new Color(230, 224, 0));
 		contentPane.add(btnExcluir, "cell 7 8,growx,aligny top");
-		
+
 		JPanel panel = new JPanel();
 		panel.setBackground(new Color(255, 255, 255));
 		contentPane.add(panel, "cell 0 4 6 8,grow");
-		
+		panel.setLayout(new MigLayout("", "[145.00px,grow]", "[82.00px,grow]"));
+
 		table = new JTable();
-		panel.add(table);
-		
+		DefaultTableModel modelo = new DefaultTableModel(new Object[][] {},
+		new String[] { "Nome da Empresa", "CNPJ", "CPF", "Telefone" });
+		table.setModel(new DefaultTableModel(new Object[][] {},
+		new String[] { "Nome da Empresa", "CNPJ", "CPF", "Telefone" }));
+		panel.add(table, "cell 0 0,grow");
+
 		JButton btnListar = new JButton("Listar");
+		btnListar.setBackground(new Color(230, 224, 0));
 		contentPane.add(btnListar, "cell 7 10,growx,aligny top");
 	}
 }
