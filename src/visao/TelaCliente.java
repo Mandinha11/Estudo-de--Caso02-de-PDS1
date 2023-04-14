05e4262f1f7ba621f18e39136bea9fa9e03f0b62
Lucas Lemes <lucas.al2003@aluno.ifsc.edu.br>
2023-04-14 08:30:18 -0300

    Tela Do Cliente

--- src/visao/TelaCliente.java
+++ src/visao/TelaCliente.java
@@ -5,10 +5,19 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JTextField;
+import java.awt.Color;
+import java.awt.Font;
+import javax.swing.JButton;
 
 public class TelaCliente extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
 
 	/**
 	 * Launch the application.
@@ -30,12 +39,92 @@ public class TelaCliente extends JFrame {
 	 * Create the frame.
 	 */
 	public TelaCliente() {
+		setTitle("Cliente");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 674, 344);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(218, 73, 73));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Nome Completo:");
+		lblNewLabel.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel.setForeground(new Color(255, 255, 255));
+		lblNewLabel.setBounds(60, 56, 100, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("CPF:");
+		lblNewLabel_1.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_1.setForeground(new Color(255, 255, 255));
+		lblNewLabel_1.setBounds(131, 95, 29, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JLabel lblNewLabel_2 = new JLabel("Data De Nascimento:");
+		lblNewLabel_2.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_2.setForeground(new Color(255, 255, 255));
+		lblNewLabel_2.setBounds(39, 147, 119, 14);
+		contentPane.add(lblNewLabel_2);
+		
+		JLabel lblNewLabel_3 = new JLabel("Numero de Telefone:");
+		lblNewLabel_3.setFont(new Font("Tahoma", Font.PLAIN, 12));
+		lblNewLabel_3.setForeground(new Color(255, 255, 255));
+		lblNewLabel_3.setBounds(41, 189, 122, 14);
+		contentPane.add(lblNewLabel_3);
+		
+		textField = new JTextField();
+		textField.setBounds(170, 92, 195, 20);
+		contentPane.add(textField);
+		textField.setColumns(10);
+		
+		JComboBox comboBox_1 = new JComboBox();
+		comboBox_1.setBounds(169, 140, 48, 22);
+		contentPane.add(comboBox_1);
+		
+		JComboBox comboBox_1_1 = new JComboBox();
+		comboBox_1_1.setBounds(227, 140, 47, 22);
+		contentPane.add(comboBox_1_1);
+		
+		JComboBox comboBox_1_2 = new JComboBox();
+		comboBox_1_2.setBounds(286, 140, 47, 22);
+		contentPane.add(comboBox_1_2);
+		
+		JLabel lblNewLabel_4 = new JLabel("Dia");
+		lblNewLabel_4.setForeground(new Color(255, 255, 255));
+		lblNewLabel_4.setBounds(170, 119, 46, 14);
+		contentPane.add(lblNewLabel_4);
+		
+		JLabel lblNewLabel_5 = new JLabel("Mês");
+		lblNewLabel_5.setForeground(new Color(255, 255, 255));
+		lblNewLabel_5.setBounds(227, 121, 46, 14);
+		contentPane.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_6 = new JLabel("Ano");
+		lblNewLabel_6.setForeground(new Color(255, 255, 255));
+		lblNewLabel_6.setBounds(284, 120, 46, 14);
+		contentPane.add(lblNewLabel_6);
+		
+		textField_1 = new JTextField();
+		textField_1.setBounds(170, 188, 185, 20);
+		contentPane.add(textField_1);
+		textField_1.setColumns(10);
+		
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.setBounds(169, 225, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Listar");
+		btnNewButton_1.setBounds(286, 225, 89, 23);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Voltar");
+		btnNewButton_2.setBounds(4, 9, 89, 23);
+		contentPane.add(btnNewButton_2);
+		
+		textField_2 = new JTextField();
+		textField_2.setBounds(169, 52, 315, 20);
+		contentPane.add(textField_2);
+		textField_2.setColumns(10);
 	}
-
 }
