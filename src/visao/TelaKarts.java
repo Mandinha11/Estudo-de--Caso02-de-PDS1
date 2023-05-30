0463de5a6ec50782bd7d9c83aeeeea950c45bff2
Lucas Henrique Santos de Bairros <lucas.hsb@aluno.ifsc.edu.br>
2023-05-30 09:07:01 -0300

    Versão Incial do JTable da TelaKarts.java

--- src/visao/TelaKarts.java
+++ src/visao/TelaKarts.java
@@ -4,7 +4,10 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.table.DefaultTableModel;
 import javax.swing.text.MaskFormatter;
 
 import controle.ClienteDAO;
@@ -13,6 +16,8 @@ import modelo.Cliente;
 import modelo.Karts;
 
 import java.awt.Color;
+import java.awt.Container;
+
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.text.ParseException;
@@ -28,6 +33,7 @@ import javax.swing.JTable;
 import javax.swing.JComboBox;
 //<<<<<<< Updated upstream;
 import javax.swing.SwingConstants;
+import javax.swing.UIManager;
 //=======
 import javax.swing.JFormattedTextField;
 //>>>>>>> Stashed changes;
@@ -39,7 +45,7 @@ public class TelaKarts extends JFrame {
 	private JTextField txtPreco_1;
 	private JTextField txtMarca;
 	private JTextField txtModelo;
-	private JTable table;
+	private JTable tabelaKarts;
 	private JTextField txtId;
 	private JTextField txtFornecedor;
 	private JTextField txtFornecedor_1;
@@ -54,7 +60,7 @@ public class TelaKarts extends JFrame {
 					TelaKarts frame = new TelaKarts();
 					frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
 					frame.setVisible(true);
-					
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -74,7 +80,7 @@ public class TelaKarts extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-		
+
 		JButton btnVoltar = new JButton("Voltar");
 		btnVoltar.setBounds(10, 11, 126, 30);
 		btnVoltar.addActionListener(new ActionListener() {
@@ -87,128 +93,216 @@ public class TelaKarts extends JFrame {
 		});
 		contentPane.setLayout(null);
 		contentPane.add(btnVoltar);
-		
+
+		tabelaKarts = new JTable();
+		tabelaKarts.setBorder(new EtchedBorder(EtchedBorder.LOWERED, UIManager.getColor("nimbusInfoBlue"),
+		        UIManager.getColor("nimbusFocus")));
+		tabelaKarts.setRowHeight(20);
+		tabelaKarts.setShowVerticalLines(true);
+		tabelaKarts.setModel(new DefaultTableModel(
+		        new Object[][] {
+		                { null, null, null, null, null, null, null, null, null },
+		                { null, null, null, null, null, null, null, null, null },
+		                { null, null, null, null, null, null, null, null, null },
+		                { null, null, null, null, null, null, null, null, null },
+		                { null, null, null, null, null, null, null, null, null },
+		                { null, null, null, null, null, null, null, null, null },
+		                { null, null, null, null, null, null, null, null, null },
+		                { null, null, null, null, null, null, null, null, null },
+		        },
+		        new String[] { "ID Kart", "Cor", "Quantidade", "Fornecedor", "Ano", "Preço", "Modelo", "Data Entrada",
+		                "Marca" }) {
+		    private static final long serialVersionUID = 1L;
+
+		    boolean[] columnEditables = new boolean[] { false, false, false, false, false, false, false };
+
+		    public boolean isCellEditable(int row, int column) {
+		        return columnEditables[column];
+		    }
+		});
+
+		JScrollPane scrollPane = new JScrollPane(tabelaKarts);
+		contentPane.add(scrollPane);
+
 		JButton btnCadastrar = new JButton("Cadastrar");
 		btnCadastrar.setBounds(54, 612, 390, 41);
 		btnCadastrar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				
+
 				Karts karts = new Karts();
-				
-				
-				KartsDAO kartsDao = KartsDAO.getinstancia();
-				
-				
-			
-				
-				 if (txtPreco.getText().trim().length() == 0) {
-					 JOptionPane.showMessageDialog(null, "Preço não preenchido!!");
-					 return;
-				 }
-				 else {
-				karts.setpreco(Long.valueOf(txtPreco.getText()));
-				 }
-			
-				 if (txtMarca.getText().trim().length() == 0) {
-					 JOptionPane.showMessageDialog(null, "Marca não preenchido!!");
-					 return;
-				 }
-				 else {
-				karts.setmarca(String.valueOf(txtMarca.getText()));
-				 }
-				 
-				 if (txtModelo.getText().trim().length() == 0) {
-					 JOptionPane.showMessageDialog(null, "Modelo não preenchido!!");
-					 return;
-				 }
-				 else {
-				karts.setmodelo(String.valueOf(txtModelo.getText()));
-				 }
-				
-				 
-				 if (txtId.getText().trim().length() == 0) {
-					 JOptionPane.showMessageDialog(null, "Id Kart não preenchido!!");
-					 return;
-				 }
-				 else {
-				karts.setId(Long.valueOf(txtId.getText()));
-				 }
-				 
-				 if (txtFornecedor.getText().trim().length() == 0) {
-					 JOptionPane.showMessageDialog(null, "CNPJ do Fornecedor não preenchido!!");
-					 return;
-				 }
-				 else {
-				karts.setforneCNPJ(Long.valueOf(txtFornecedor.getText()));
-				 }
-				 
-				 
-				 
+
+				KartsDAO KartsDao = KartsDAO.getinstancia();
+
+				if (txtPreco.getText().trim().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Preço não preenchido!!");
+					return;
+				} else {
+					karts.setpreco(Long.valueOf(txtPreco.getText()));
+				}
+
+				if (txtMarca.getText().trim().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Marca não preenchido!!");
+					return;
+				} else {
+					karts.setmarca(String.valueOf(txtMarca.getText()));
+				}
+
+				if (txtModelo.getText().trim().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Modelo não preenchido!!");
+					return;
+				} else {
+					karts.setmodelo(String.valueOf(txtModelo.getText()));
+				}
+
+				if (txtId.getText().trim().length() == 0) {
+					JOptionPane.showMessageDialog(null, "Id Kart não preenchido!!");
+					return;
+				} else {
+					karts.setId(Long.valueOf(txtId.getText()));
+				}
+
+				if (txtFornecedor.getText().trim().length() == 0) {
+					JOptionPane.showMessageDialog(null, "CNPJ do Fornecedor não preenchido!!");
+					return;
+				} else {
+					karts.setforneCNPJ(Long.valueOf(txtFornecedor.getText()));
+				}
 			}
 		});
 		btnCadastrar.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
 		contentPane.add(btnCadastrar);
-		
+
+		JLabel lblBuscarKarts = new JLabel("Cor:");
+		lblBuscarKarts.setBounds(100, 100, 67, 30);
+		contentPane.add(lblBuscarKarts);
+		lblBuscarKarts.setForeground(new Color(0, 0, 0));
+		lblBuscarKarts.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
+		JLabel lblQuantidade = new JLabel("Quantidade:");
+		lblQuantidade.setBounds(480, 100, 104, 30);
+		contentPane.add(lblQuantidade);
+		lblQuantidade.setForeground(new Color(0, 0, 0));
+		lblQuantidade.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
+		JLabel lblMotor = new JLabel("Motor:");
+		lblMotor.setBounds(80, 166, 104, 30);
+		contentPane.add(lblMotor);
+		lblMotor.setForeground(Color.BLACK);
+		lblMotor.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
 		MaskFormatter mascaraCNPJ = null;
 		try {
 			mascaraCNPJ = new MaskFormatter("##.###.###/000#-##");
 		} catch (ParseException e) {
 			e.printStackTrace();
 		}
-		
+
 		txtFornecedor = new JFormattedTextField(mascaraCNPJ);
-		
+
+		txtFornecedor = new JTextField();
+		txtFornecedor.setBounds(596, 169, 260, 30);
+		contentPane.add(txtFornecedor);
+		txtFornecedor.setColumns(10);
+
+		txtId = new JTextField();
+		txtId.setBounds(165, 253, 260, 30);
+		contentPane.add(txtId);
+		txtId.setColumns(10);
+
+		JLabel lblIdKart = new JLabel("ID Kart:");
+		lblIdKart.setBounds(69, 250, 104, 30);
+		contentPane.add(lblIdKart);
+		lblIdKart.setForeground(Color.BLACK);
+		lblIdKart.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
+		JLabel lblAno = new JLabel("Ano:");
+		lblAno.setBounds(525, 250, 59, 30);
+		contentPane.add(lblAno);
+		lblAno.setForeground(new Color(0, 0, 0));
+		lblAno.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
 		txtPreco = new JTextField();
 		MaskFormatter formatter = null;
 		try {
 			formatter = new MaskFormatter("R$ ###.###,##");
-		}catch(ParseException e2) {
+		} catch (ParseException e2) {
 			e2.printStackTrace();
 		}
-		
-		
-		JButton btnExcluir = new JButton("Excluir");
-		btnExcluir.setBounds(524, 612, 386, 41);
-		btnExcluir.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		contentPane.add(btnExcluir);
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(20, 124, 903, 429);
-		panel_1.setBackground(new Color(211, 211, 211));
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		JLabel lblDataEntrada = new JLabel("Data Entrada:");
-		lblDataEntrada.setBounds(40, 259, 104, 30);
-		panel_1.add(lblDataEntrada);
-		lblDataEntrada.setForeground(Color.BLACK);
-		lblDataEntrada.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		JComboBox boxANO = new JComboBox();
-		boxANO.setBounds(330, 259, 82, 22);
-		panel_1.add(boxANO);
-		
-		boxANO.addItem("2023");
-		
-		JComboBox boxMes = new JComboBox();
-		boxMes.setBounds(244, 259, 82, 22);
-		panel_1.add(boxMes);
-		boxMes.addItem("01");
-		boxMes.addItem("02");
-		boxMes.addItem("03");
-		boxMes.addItem("04");
-		boxMes.addItem("05");
-		boxMes.addItem("06");
-		boxMes.addItem("07");
-		boxMes.addItem("08");
-		boxMes.addItem("09");
-		boxMes.addItem("10");
-		boxMes.addItem("11");
-		boxMes.addItem("12");
-		
-		JComboBox boxDia = new JComboBox();
-		boxDia.setBounds(154, 259, 80, 22);
-		panel_1.add(boxDia);
+		txtPreco = new JFormattedTextField(formatter);
+
+		txtPreco.setBounds(164, 343, 261, 30);
+		contentPane.add(txtPreco);
+		txtPreco.setColumns(10);
+
+		JLabel lblPreo = new JLabel("Preço:");
+		lblPreo.setBounds(80, 340, 67, 30);
+		contentPane.add(lblPreo);
+		lblPreo.setForeground(new Color(0, 0, 0));
+		lblPreo.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
+		txtModelo = new JTextField();
+		txtModelo.setBounds(596, 343, 260, 30);
+		contentPane.add(txtModelo);
+		txtModelo.setColumns(10);
+
+		JLabel lblModelo = new JLabel("Modelo:");
+		lblModelo.setBounds(505, 340, 67, 30);
+		contentPane.add(lblModelo);
+		lblModelo.setForeground(Color.BLACK);
+		lblModelo.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
+		txtMarca = new JTextField();
+		txtMarca.setBounds(597, 418, 259, 30);
+		contentPane.add(txtMarca);
+		txtMarca.setColumns(10);
+
+		JLabel lblMarcaKarts = new JLabel("Marca:");
+		lblMarcaKarts.setBounds(517, 415, 67, 30);
+		contentPane.add(lblMarcaKarts);
+		lblMarcaKarts.setForeground(new Color(0, 0, 0));
+		lblMarcaKarts.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
+		JComboBox<String> boxCor = new JComboBox<String>();
+		boxCor.setBounds(165, 105, 262, 26);
+		contentPane.add(boxCor);
+		boxCor.addItem("Selecionar Cor");
+		boxCor.addItem("Amarelo");
+		boxCor.addItem("Vermelho");
+		boxCor.addItem("Azul");
+		boxCor.addItem("Verde");
+		boxCor.addItem("Rosa");
+		boxCor.addItem("Roxo");
+		boxCor.addItem("Preto");
+		boxCor.addItem("Branco");
+		boxCor.addItem("Cinza");
+
+		JComboBox<String> boxQuantidade = new JComboBox<String>();
+		boxQuantidade.setBounds(594, 104, 262, 29);
+		contentPane.add(boxQuantidade);
+		boxQuantidade.addItem("Selecinar Quantidade");
+		boxQuantidade.addItem("1");
+		boxQuantidade.addItem("2");
+		boxQuantidade.addItem("3");
+		boxQuantidade.addItem("4");
+		boxQuantidade.addItem("5");
+		boxQuantidade.addItem("6");
+		boxQuantidade.addItem("7");
+		boxQuantidade.addItem("8");
+		boxQuantidade.addItem("9");
+		boxQuantidade.addItem("10");
+
+		JLabel lblNewLabel = new JLabel("Fornecedor:");
+		lblNewLabel.setBounds(480, 170, 100, 22);
+		lblNewLabel.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+		contentPane.add(lblNewLabel);
+
+		JComboBox<Object> boxAno = new JComboBox<Object>();
+		boxAno.setBounds(596, 257, 260, 22);
+		contentPane.add(boxAno);
+
+		JComboBox<String> boxDia = new JComboBox<String>();
+		boxDia.setBounds(165, 422, 80, 22);
+		contentPane.add(boxDia);
 		boxDia.addItem("01");
 		boxDia.addItem("02");
 		boxDia.addItem("03");
@@ -240,136 +334,47 @@ public class TelaKarts extends JFrame {
 		boxDia.addItem("29");
 		boxDia.addItem("30");
 		boxDia.addItem("31");
-		txtPreco_1 = new JFormattedTextField(formatter);
-		txtPreco_1.setBounds(151, 199, 261, 30);
-		panel_1.add(txtPreco_1);
-		txtPreco_1.setColumns(10);
-		
-		JLabel lblPreo = new JLabel("Preço:");
-		lblPreo.setBounds(74, 196, 67, 30);
-		panel_1.add(lblPreo);
-		lblPreo.setForeground(new Color(0, 0, 0));
-		lblPreo.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		txtId = new JTextField();
-		txtId.setBounds(151, 71, 260, 30);
-		panel_1.add(txtId);
-		txtId.setColumns(10);
-		
-		JLabel lblIdKart = new JLabel("ID Kart:");
-		lblIdKart.setBounds(74, 71, 104, 30);
-		panel_1.add(lblIdKart);
-		lblIdKart.setForeground(Color.BLACK);
-		lblIdKart.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		JComboBox boxCor = new JComboBox();
-		boxCor.setBounds(150, 138, 262, 26);
-		panel_1.add(boxCor);
-		boxCor.addItem("Selecionar Cor");
-		boxCor.addItem("Amarelo");
-		boxCor.addItem("Vermelho");
-		boxCor.addItem("Azul");
-		boxCor.addItem("Verde");
-		boxCor.addItem("Rosa");
-		boxCor.addItem("Roxo");
-		boxCor.addItem("Preto");
-		boxCor.addItem("Branco");
-		boxCor.addItem("Cinza");
-		
-		JLabel lblBuscarKarts = new JLabel("Cor:");
-		lblBuscarKarts.setBounds(91, 138, 67, 30);
-		panel_1.add(lblBuscarKarts);
-		lblBuscarKarts.setForeground(new Color(0, 0, 0));
-		lblBuscarKarts.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		txtMarca = new JTextField();
-		txtMarca.setBounds(151, 318, 259, 30);
-		panel_1.add(txtMarca);
-		txtMarca.setColumns(10);
-		
-		JLabel lblMarcaKarts = new JLabel("Marca:");
-		lblMarcaKarts.setBounds(74, 315, 67, 30);
-		panel_1.add(lblMarcaKarts);
-		lblMarcaKarts.setForeground(new Color(0, 0, 0));
-		lblMarcaKarts.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		txtModelo = new JTextField();
-		txtModelo.setBounds(590, 262, 260, 30);
-		panel_1.add(txtModelo);
-		txtModelo.setColumns(10);
-		
-		JLabel lblModelo = new JLabel("Modelo:");
-		lblModelo.setBounds(513, 259, 67, 30);
-		panel_1.add(lblModelo);
-		lblModelo.setForeground(Color.BLACK);
-		lblModelo.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		JComboBox boxAno = new JComboBox();
-		boxAno.setBounds(589, 199, 260, 30);
-		panel_1.add(boxAno);
-		
-		JLabel lblAno = new JLabel("Ano:");
-		lblAno.setBounds(521, 196, 59, 30);
-		panel_1.add(lblAno);
-		lblAno.setForeground(new Color(0, 0, 0));
-		lblAno.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		txtFornecedor_1 = new JTextField();
-		txtFornecedor_1.setBounds(589, 136, 260, 30);
-		panel_1.add(txtFornecedor_1);
-		txtFornecedor_1.setColumns(10);
-		
-		JLabel lblNewLabel = new JLabel("Fornecedor CNPJ:");
-		lblNewLabel.setBounds(436, 137, 144, 22);
-		panel_1.add(lblNewLabel);
-		lblNewLabel.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		
-		JComboBox boxQuantidade = new JComboBox();
-		boxQuantidade.setBounds(590, 72, 262, 29);
-		panel_1.add(boxQuantidade);
-		boxQuantidade.addItem("Selecinar Quantidade");
-		boxQuantidade.addItem("1");
-		boxQuantidade.addItem("2");
-		boxQuantidade.addItem("3");
-		boxQuantidade.addItem("4");
-		boxQuantidade.addItem("5");
-		boxQuantidade.addItem("6");
-		boxQuantidade.addItem("7");
-		boxQuantidade.addItem("8");
-		boxQuantidade.addItem("9");
-		boxQuantidade.addItem("10");
-		
-		JLabel lblQuantidade = new JLabel("Quantidade:");
-		lblQuantidade.setBounds(486, 71, 104, 30);
-		panel_1.add(lblQuantidade);
-		lblQuantidade.setForeground(new Color(0, 0, 0));
-		lblQuantidade.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
-		
-		JPanel panel_3 = new JPanel();
-		panel_3.setBackground(new Color(0, 128, 128));
-		panel_3.setBounds(20, 550, 903, 131);
-		contentPane.add(panel_3);
-		panel_3.setLayout(null);
-		
-		JPanel panel_4 = new JPanel();
-		panel_4.setBackground(new Color(211, 211, 211));
-		panel_4.setBounds(996, 54, 857, 939);
-		contentPane.add(panel_4);
-		panel_4.setLayout(null);
-		
-		Panel panel = new Panel();
-		panel.setBounds(10, 10, 837, 919);
-		panel_4.add(panel);
-		panel.setBackground(new Color(255, 255, 255));
-		panel.setForeground(new Color(255, 255, 255));
-		panel.setLayout(null);
-		
-		table = new JTable();
-		table.setBounds(368, 161, 1, 1);
-		panel.add(table);
-		
-		JLabel lblNewLabel_1 = new JLabel("New label");
+
+		JComboBox<String> boxMes = new JComboBox<String>();
+		boxMes.setBounds(255, 422, 82, 22);
+		contentPane.add(boxMes);
+		boxMes.addItem("01");
+		boxMes.addItem("02");
+		boxMes.addItem("03");
+		boxMes.addItem("04");
+		boxMes.addItem("05");
+		boxMes.addItem("06");
+		boxMes.addItem("07");
+		boxMes.addItem("08");
+		boxMes.addItem("09");
+		boxMes.addItem("10");
+		boxMes.addItem("11");
+		boxMes.addItem("12");
+
+		JComboBox<String> boxANO = new JComboBox<String>();
+		boxANO.setBounds(347, 422, 82, 22);
+		contentPane.add(boxANO);
+
+		boxANO.addItem("2023");
+
+		JButton btnExcluir = new JButton("Excluir");
+		btnExcluir.setBounds(524, 612, 386, 41);
+		btnExcluir.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+		contentPane.add(btnExcluir);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(20, 57, 903, 501);
+		panel_1.setBackground(new Color(211, 211, 211));
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+
+		JLabel lblDataEntrada = new JLabel("Data Entrada:");
+		lblDataEntrada.setBounds(27, 358, 104, 30);
+		panel_1.add(lblDataEntrada);
+		lblDataEntrada.setForeground(Color.BLACK);
+		lblDataEntrada.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 16));
+
+		JLabel lblNewLabel_1 = new JLabel("");
 		lblNewLabel_1.setIcon(new ImageIcon(TelaKarts.class.getResource("/imgs/FundoDeTela.jpg")));
 		lblNewLabel_1.setBounds(0, 0, 1924, 1061);
 		contentPane.add(lblNewLabel_1);
